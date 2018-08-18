89c2517b3bebd45c497280babc1bbaf0e6dca5d4
tamas.czerjak <tamas.czerjak@icellmobilsoft.hu>
2018-08-18 11:04:26 +0200

    project structure

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './app/home/home.component';
+import { GameComponent } from './app/game/game.component';
+import { PageNotFoundComponent } from './app/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+    { path: 'game', component: GameComponent },
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './app/home/home.component';
+import { HeaderComponent } from './app/header/header.component';
+import { GameComponent } from './app/game/game.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { PageNotFoundComponent } from './app/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    GameComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
